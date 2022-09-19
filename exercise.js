41f6fe431948b5ded69032383c57755261e897f8
pajith551@gmail.com <pajith551@gmail.com>
2022-09-19 13:59:53 +0530

    exercise

--- exercise.js
+++ exercise.js
@@ -50,7 +50,68 @@ let numm = parseInt(num);
 console.log(typeof(numm));
 console.log( numm == 10);
 
+let par = "9.8";
+let par1 = Math.round(par)
+console.log(par1 == 10);
 
+let match1 = "python , jargon";
+console.log(match1.match(/on/g));
 
+let match2 = "Match the match with the Matching";
+console.log(match2.match(/mat/gi));
 
+let match3 = "I hope this course is not full of jargon";
+console.log(match3.match("jargon"));
+
+let rand1 = Math.random();
+let rand100 = rand1*100;
+console.log(Math.ceil(rand100));
+
+let rand2 = Math.random();
+let rand200 = rand2*255;
+console.log(Math.ceil(rand200));
+
+let min1 = 50;
+let max1 = 100;
+console.log(Math.ceil(Math.random()* (max1 - min1)+min1));
+console.log(Math.ceil(Math.random()*(5 -2)+2));
+
+let script = "JavaScript";
+console.log(script.charAt(8));
+
+let stringLast = script.length-1;
+console.log(script.charAt(stringLast));
+console.log(script.charAt(0));
+console.log(script.charAt(1));
+console.log(script.charAt(2));
+console.log(script.charAt(3));
+console.log(script.charAt(4));
+console.log(script.charAt(5));
+console.log(script.charAt(6));
+console.log(script.charAt(7));
+console.log(script.charAt(8));
+console.log(script.charAt(script.length-1));
+
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
+
+let slice = "You cannot end a sentence with because because because is a conjunction";
+console.log(slice.substring(31,55));
+
+let countLove ="Love is the best thing in this world. Some found their love and some are still looking for their love";
+console.log(countLove.match(/love/gi));
+console.log("love count is: " + countLove.match(/love/gi).length);
+
+console.log("total string count in countLove: " + countLove.length)
+
+let becauseCount = "You cannot end a sentence with because because because is a conjunction";
+let counting = /because/gi;
+console.log(becauseCount.match(counting));
+
+console.log("because count is: " + becauseCount.match(counting).length);
+
+let freq = "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+
+let rep1 =/\W+/g;
+let rep2 = " ";
+console.log(freq.replace(rep1,rep2));
 
