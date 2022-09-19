8e8d963d0e750b1e7654bef04bf2cad3ac14911d
pajith551@gmail.com <pajith551@gmail.com>
2022-09-19 11:20:39 +0530

    exercise

---
+++ exercise.js
@@ -0,0 +1,56 @@
+let challenge = "30 days of JavaScript";
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substring(0,2));
+console.log(challenge.substring(3, 21));
+console.log(challenge.includes("Script"));
+console.log(challenge.split());
+console.log(challenge.split(" "));
+
+let company = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+console.log(company.split());
+console.log(company.split(","))
+
+console.log(challenge.replace("JavaScript", "Python"));
+
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt("J"));
+
+console.log(challenge.indexOf("a"));
+console.log(challenge.lastIndexOf("a"));
+
+let because ="You cannot end a sentence with because because because is a conjunction";
+console.log(because.indexOf("because"));
+console.log(because.lastIndexOf("because"));
+
+console.log(because.search("because"));
+
+let trim1 = " 30 Days Of JavaScript ";
+console.log(trim1.trim());
+console.log(challenge.startsWith(30));
+console.log(challenge.endsWith("of JavaScript"));
+console.log(challenge.match(/a/gi));
+
+let con = "30 days of";
+console.log(con.concat(" JavaScript"));
+
+console.log(challenge.repeat(2));
+
+let print1 = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
+console.log(print1);
+
+let print2 = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+console.log(print2);
+
+let num = "10";
+console.log(typeof(num));
+let numm = parseInt(num);
+console.log(typeof(numm));
+console.log( numm == 10);
+
+
+
+
+
